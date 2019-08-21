243d209d12599f017c9a7cdb1b0cf0acfc639e1b
Evan Tahler <evan.tahler@voom.flights>
2019-08-21 12:54:01 -0700

    pictures and delayed page

--- public-src/router.js
+++ public-src/router.js
@@ -2,12 +2,10 @@ import React from 'react'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import Client from './components/client'
 
-// import Page from './components/page.jsx'
-
 import Overview from './pages/overview'
 import Failed from './pages/failed'
 import Workers from './pages/workers'
-// import Delayed from './delayed.jsx'
+import Delayed from './pages/delayed'
 import Queue from './pages/queue'
 import Locks from './pages/locks'
 import Redis from './pages/redis'
@@ -25,9 +23,8 @@ function ApplicationRouter () {
         <Route exact path='/' render={(props) => <Overview {...props} client={client} />} />
         <Route exact path={['/queue/:queue', '/queue/:queue/:page']} render={(props) => <Queue {...props} client={client} />} />
         <Route exact path={['/failed', '/failed/:page']} render={(props) => <Failed {...props} client={client} />} />
+        <Route exact path={['/delayed', '/delayed/:page']} render={(props) => <Delayed {...props} client={client} />} />
         <Route exact path='/workers' render={(props) => <Workers {...props} client={client} />} />
-        {/* <Route path='delayed' component={Delayed} /> */}
-        {/* <Route path='delayed/:page' component={Delayed} /> */}
         <Route exact path='/locks' render={(props) => <Locks {...props} client={client} />} />
         <Route exact path='/redis' render={(props) => <Redis {...props} client={client} />} />
         <Route component={PageNotFound} />
