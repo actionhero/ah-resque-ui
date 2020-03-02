e5377bd82dd98a57cf84e5b6e37a03ef7b3a02c8
Evan Tahler <evan@evantahler.com>
2020-03-01 18:16:24 -0800

    lowercase import

--- public-src/components/bumpChart.jsx
+++ public-src/components/bumpChart.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { ResponsiveAreaBump } from '@nivo/Bump'
-import { Spinner, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { ResponsiveAreaBump } from "@nivo/bump";
+import { Spinner, Row, Col } from "react-bootstrap";
 
-function BumpChart ({ data }) {
+function BumpChart({ data }) {
   if (!data[0] || data[0].data.length < 2) {
     return (
       <Row>
-        <Col md={12} style={{ textAlign: 'center', paddingTop: 100 }}>
-          <Spinner animation='border' />
+        <Col md={12} style={{ textAlign: "center", paddingTop: 100 }}>
+          <Spinner animation="border" />
         </Col>
       </Row>
-    )
+    );
   }
 
   return (
@@ -18,27 +18,27 @@ function BumpChart ({ data }) {
       data={data}
       margin={{ top: 40, right: 100, bottom: 40, left: 100 }}
       spacing={8}
-      colors={{ scheme: 'category10' }}
-      blendMode='multiply'
+      colors={{ scheme: "category10" }}
+      blendMode="multiply"
       animate={false}
       axisTop={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: '',
-        legendPosition: 'middle',
+        legend: "",
+        legendPosition: "middle",
         legendOffset: -36
       }}
       axisBottom={{
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: '',
-        legendPosition: 'middle',
+        legend: "",
+        legendPosition: "middle",
         legendOffset: 32
       }}
     />
-  )
+  );
 }
 
-export default BumpChart
+export default BumpChart;
